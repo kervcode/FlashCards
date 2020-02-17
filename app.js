3abb7a819ab96bed799a44a964b2b51cabccab6b
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2020-02-17 10:45:42 -0500

    setup

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const express = require('express');
+
+const app = express() //return an express application.
+
+app.get(); //create a route for the root route the the flashcard app.
+
+app.listen(3000) //listen method is use to setup the dev setup. It took one param {the port number}.
