81db97ef5de9d6a0295e8f8c761f9ca0135f56dd
Kervcodes-desktop <Kervintz84@hotmail.com>
2020-03-05 20:23:25 -0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express() //return an express application.
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get(); //create a route for the root route the the flashcard app.
+app.set("view engine", "pug");
 
-app.listen(3000) //listen method is use to setup the dev setup. It took one param {the port number}.
+app.get("/", (req, res) => {
+  res.send("I am going over this for a 3rd time!");
+});
+
+app.get("/cards", (req, res) => {
+  res.render("card", {
+    prompt: "Who is buried in Grant's tomb?"
+  });
+
+  // res.locals.prompt = "Who is buried in Grant's Thumb?";
+  // res.render("card");
+});
+
+app.get("/hello", (req, res) => {
+  res.render("hello");
+});
+
+app.post("/hello", (req, res) => {
+  // console.dir(req.body);
+  // res.render("hello");
+  res.json(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000.");
+});
